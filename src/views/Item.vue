87909efb542f8ce9cbe952290f7267d596033a46
krystal-star <52444940+krystal-star@users.noreply.github.com>
2021-03-23 23:09:42 +0000

    update itemInfo

--- src/views/Item.vue
+++ src/views/Item.vue
@@ -17,15 +17,50 @@
                 </div>
 
                 <div class="description">
-
+                    <h2>商品介绍</h2>
+                    <p>A dynamic panel contains a state for each slide. The arrows have Click interactions that set the dynamic panel to its Next or Previous state with a Fade animation. The arrows also update the text on the slide counter.
+                    </p>
+                    <p>The dynamic panel has a Panel State Changed interaction that uses conditional case logic to disable the left arrow when the panel's first state is showing and the right arrow when the panel's final state is showing.
+                    </p>
+                    <p>The dynamic panel also has Swipe interactions that Fire the Click events of the arrows, allowing users to interact with the slideshow on mobile devices.
+                    </p>
                 </div>
 
                 <div class="reviews">
-
+                    <h2>用户评论 <span class="total-num"> 共158条评论</span></h2>
                 </div>
 
                 <div class="recommend">
+                    <p class="text">猜你喜欢</p>
+                    <el-carousel height="400px" indicator-position="outside">
+                        <el-carousel-item :key="1">
+                            <el-row :gutter="20">
+                                <el-col :span="8" v-for="(item,index) in discount">
+                                    <div class="lunbo" v-if="index < 3">
+                                        <el-image :src="item.img" class="image"></el-image>
+                                        <p class="name" @mouseover="changeColor($event,3)" @mouseleave="recoverColor($event,3)">{{item.name}}</p>
+                                        <p class="pre-price">¥{{item.previousPrice}}</p>
+                                        <p class="cur-price" @mouseover="changeColor($event,3)" @mouseleave="recoverColor($event,3)">¥{{item.price}}</p>
+                                        <p class="group-style">{{item.group}}{{item.style}}</p>
+                                    </div>
+                                </el-col>
+                            </el-row>
+                        </el-carousel-item>
 
+                        <el-carousel-item :key="2">
+                            <el-row :gutter="20">
+                                <el-col :span="8" v-for="(item,index) in discount">
+                                    <div class="lunbo" v-if="index >= 3">
+                                        <el-image :src="item.img" class="image"></el-image>
+                                        <p class="name" @mouseover="changeColor($event,3)" @mouseleave="recoverColor($event,3)">{{item.name}}</p>
+                                        <p class="pre-price">¥{{item.previousPrice}}</p>
+                                        <p class="cur-price" @mouseover="changeColor($event,3)" @mouseleave="recoverColor($event,3)">¥{{item.price}}</p>
+                                        <p class="group-style">{{item.group}}{{item.style}}</p>
+                                    </div>
+                                </el-col>
+                            </el-row>
+                        </el-carousel-item>
+                    </el-carousel>
                 </div>
             </el-main>
 
@@ -59,7 +94,59 @@
                     price: 1499,
                     previousPrice: null,
                     brand: "Nike"
-                }
+                },
+                discount: [
+                    {
+                        id: 8,
+                        brand: "Nike",
+                        name: "Sportswear Essentials",
+                        img: "../static/sportswear-essentials-backpack-0.jpg",
+                        price: 320,
+                        previousPrice: 499,
+                        group: "男子",
+                        style: "背包",
+                    },
+                    {
+                        id: 6,
+                        brand: "Adidas",
+                        name: "Tensaur",
+                        img: "../static/Tensaur_Shoes_Black-0.jpg",
+                        price: 210,
+                        previousPrice: 240,
+                        group: "儿童",
+                        style: "运动鞋",
+                    },
+                    {
+                        id: 5,
+                        brand: "Adidas",
+                        name: "HER Studio London",
+                        img: "../static/HER_Studio_London_T-Shirt-0.jpg",
+                        price: 200,
+                        previousPrice: 290,
+                        group: "女子",
+                        style: "T恤",
+                    },
+                    {
+                        id: 4,
+                        brand: "Nike",
+                        name: "Air Max 90",
+                        img: "../static/air-max-90-eoi-0.jpg",
+                        price: 390,
+                        previousPrice: 490,
+                        group: "男子",
+                        style: "运动鞋",
+                    },
+                    {
+                        id: 6,
+                        brand: "Adidas",
+                        name: "Tensaur",
+                        img: "../static/Tensaur_Shoes_Black-0.jpg",
+                        price: 210,
+                        previousPrice: 240,
+                        group: "儿童",
+                        style: "运动鞋",
+                    }
+                ],
             }
         },
         methods:{
@@ -79,12 +166,39 @@
                 if(target.className.toLowerCase() === "el-image__inner"){
                     oneImg.src = target.src;
                 }
+            },
+            changeColor: function(e, val) {
+                let target = e.target;
+                let parent_node = target.parentNode;
+                var changeNode;
+                if(target.className.toLowerCase() === "name"){
+                    changeNode = parent_node.childNodes[val];
+                }else{
+                    changeNode = parent_node.childNodes[1];
+                }
+                target.style = "opacity:0.5";
+                changeNode.style = "opacity:0.5";
+            },
+            recoverColor: function (e,val) {
+                let target = e.target;
+                let parent_node = target.parentNode
+                var changeNode;
+                if(target.className.toLowerCase() === "name"){
+                    changeNode = parent_node.childNodes[val];
+                }else{
+                    changeNode = parent_node.childNodes[1];
+                }
+                target.style = "opacity:1";
+                changeNode.style = "opacity:1";
             }
         }
     }
 </script>
 
 <style scoped>
+    div.show-imgs{
+        display: flex;
+    }
     div.three-imgs{
         width: 10%;
         height: 500px;
@@ -114,5 +228,63 @@
 
     .border{
         border-style: solid;
+    }
+
+    div.description{
+        background-color: rgba(144, 147, 153, 0.2);
+        margin: 30px;
+        padding: 120px;
+    }
+
+    div.description p{
+        text-align: left;
+    }
+
+    div.reviews{
+        background-color: rgba(144, 147, 153, 0.2);
+        margin: 30px;
+        padding: 30px;
+    }
+
+    div.reviews h2{
+        text-align: left;
+    }
+
+    .total-num{
+        font-size: 13px;
+        font-weight: normal;
+        padding: 10px;
+        color: #909399;
+    }
+
+    p.text{
+        font-weight: bold;
+        font-size: 26px;
+        display: inline-block;
+    }
+    p.pre-price, p.cur-price{
+        float:right;
+    }
+    p.name, p.pre-price, p.cur-price{
+        font-weight: bold;
+        font-size: 18px;
+        display: inline-block;
+    }
+    p.group-style{
+        margin-top: 0px;
+        color: #909399;
+    }
+    .image, p.name:hover, p.cur-price:hover{
+        cursor: pointer;
+    }
+    p.pre-price{
+        color: #909399;
+        text-decoration: line-through;
+        font-size: 15px;
+        padding-left: 5px;
+    }
+
+    div.lunbo{
+        text-align: left;
     }
 </style>
